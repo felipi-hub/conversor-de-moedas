b975eba8a174e809a6805663a764100f31117afe
felipi <felipi.delfino@hotmail.com>
2025-08-25 11:56:44 -0300

    Atualizando a funcao filtro de historico para ficar performatico

--- src/app/feature/history-conversion/history-conversion.component.ts
+++ src/app/feature/history-conversion/history-conversion.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Transaction } from '../../shared/interfaces/transaction.interface';
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { ConversionHistoryService } from '../../shared/services/conversion-history.service';
@@ -10,22 +10,28 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 import { ViewChild, AfterViewInit } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, map, startWith, Subject, takeUntil } from 'rxjs';
 
 
 
 @Component({
   selector: 'app-history-conversion',
   standalone: true,
-  imports: [DecimalPipe,
+  imports: [DecimalPipe, ReactiveFormsModule,
     DatePipe, SharedModule, MatPaginatorModule],
   templateUrl: './history-conversion.component.html',
   styleUrls: ['./history-conversion.component.scss']
 })
-export class HistoryConversionComponent implements OnInit, AfterViewInit {
+export class HistoryConversionComponent implements OnInit, AfterViewInit, OnDestroy {
 
   transactions: Transaction[] = [];
   displayedColumns: string[] = ['id', 'currencies', 'valueConverted', 'dateTime', 'actions'];
   dataSource = new MatTableDataSource<Transaction>([]);
+
+  filterCtrl = new FormControl<string>('', { nonNullable: true });
+  private destroy$ = new Subject<void>();
+
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
 
@@ -45,10 +51,29 @@ export class HistoryConversionComponent implements OnInit, AfterViewInit {
       };
 
     });
+    this.filterCtrl.valueChanges.pipe(
+      startWith(this.filterCtrl.value),
+      debounceTime(300),
+      map(v => (v ?? '').toString().trim().toLowerCase()),
+      distinctUntilChanged(),
+      takeUntil(this.destroy$)
+    ).subscribe(value => {
+      console.log("filter", value)
+      this.dataSource.filter = value;
+      if (this.dataSource.paginator) {
+        this.dataSource.paginator.firstPage();
+      }
+    });
+
   }
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
   applyFilter(event: Event): void {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
